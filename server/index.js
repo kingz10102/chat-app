e160dc431d0650af44e53006049b20ad1768c587
Kok Kok <REMY@Koks-MacBook-Pro.local>
2020-08-13 19:39:52 -0400

    this a  commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+server.listen()
