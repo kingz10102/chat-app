43b02a832df9908b383326bec501e78937be4efc
Kok Kok <REMY@Koks-MacBook-Pro.local>
2020-08-13 20:46:21 -0400

    thisis a  commit

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -2,11 +2,21 @@ import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
-const Chat = () => {
+
+
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+
     useEffect(() => {
-        const data = queryString.parse(location.search);
-        
-    })
+        const { name, room } = queryString.parse(location.search);
+
+        setName(name);
+        setName(room);
+
+    });
     
     return (
         <h1>Chat</h1>
